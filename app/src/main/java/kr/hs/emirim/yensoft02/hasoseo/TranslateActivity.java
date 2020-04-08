802bd8ddd63afe79f041b0ea2e0655e4c35ad631
KimYeeun <s2018s01@e-mirim.hs.kr>
2020-04-09 02:55:46 +0900

    Make Theme & Success ActionBar

---
+++ app/src/main/java/kr/hs/emirim/yensoft02/hasoseo/TranslateActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.yensoft02.hasoseo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TranslateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translate);
+    }
+}
