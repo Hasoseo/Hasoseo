ef43feda26b84fd72c05b6fa793e952f64a942df
KimYeeun <s2018s01@e-mirim.hs.kr>
2020-05-03 17:47:22 +0900

    Write_frag를 list_frag로 교체

--- app/src/main/java/kr/hs/emirim/yensoft02/hasoseo/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/yensoft02/hasoseo/MainActivity.java
@@ -2,6 +2,7 @@ package kr.hs.emirim.yensoft02.hasoseo;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private FragmentManager fm;
     private FragmentTransaction ft;
     private Translate_before_frag tbf; // 교정들어가기 전(교정1) fragment 객체
-    private Write_frag wf; // 작성 fragment 객체
+    private List_frag lf; // 작성 fragment 객체
     private Setting_frag sf; // 환경설정 fragment 객체
 
     @Override
@@ -43,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         tbf = new Translate_before_frag();
-        wf = new Write_frag();
+        lf = new List_frag();
         sf = new Setting_frag();
         setFrag(0); // 첫 프래그먼트 화면 지정
 
@@ -59,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 ft.commit(); // transaction 커밋
                 break;
             case 1:
-                ft.replace(R.id.MainFrame, wf);
+                ft.replace(R.id.MainFrame, lf);
                 ft.commit();
                 break;
             case 2:
@@ -68,4 +69,10 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+    public void replaceFragment(Fragment fragment) {
+        fm = getSupportFragmentManager(); // 프래그먼트를 추가,삭제 또는 교체등의 작업
+        ft = fm.beginTransaction(); // transaction 처음
+        ft.replace(R.id.MainFrame, fragment).commit();      // Fragment로 사용할 MainActivity내의 layout공간을 선택합니다.
+    }
+
 }
