d729d9796f1b64f0693d2f4314699f0ecc3cf7b9
MoonHyekyung <moonsoft02@gmail.com>
2020-05-03 12:39:12 +0900

    implement Favorites button add star_checked.xml

--- app/src/main/java/kr/hs/emirim/yensoft02/hasoseo/Translate_before_frag.java
+++ app/src/main/java/kr/hs/emirim/yensoft02/hasoseo/Translate_before_frag.java
@@ -16,7 +16,7 @@ public class Translate_before_frag extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_translate, container, false); // false는 뷰를 수신하는 모든 터치 이벤트도 상위 뷰로 전송하는 것등 무시
-        // inflater로 view 생성 (인플레이트라는 것은 xml 파일을 통해서 객체화)
+        // inflater로 view 생성 (인플레이트`라는 것은 xml 파일을 통해서 객체화)
 
         return view;
     }
