e47e5e02ee45d9e7dd83c1ca59772cb7fedd1b7c
MoonHyekyung <moonsoft02@gmail.com>
2020-05-11 16:17:07 +0900

    Connect LogupActivity to FirebaseDatabase FirebaseDatabase: use realtime-database(nickname, email) Implement check password Implement check nickname

--- app/src/main/java/kr/hs/emirim/yensoft02/hasoseo/LogupActivity.java
+++ app/src/main/java/kr/hs/emirim/yensoft02/hasoseo/LogupActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,62 +21,42 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class LogupActivity extends AppCompatActivity implements Initialize{
+import java.util.ArrayList;
+
+import kr.hs.emirim.yensoft02.hasoseo.model.User;
+
+public class LogupActivity extends AppCompatActivity implements Initialize, View.OnClickListener{
     private TextView login_btn;
+    private EditText nickname_info;
     private EditText email_info;
     private EditText pwd_info;
+    private EditText pwd_check;
     private Button btn;
-    FirebaseAuth user;
+
+    private FirebaseAuth user;
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
+    private ArrayList<String> nicknameData = new ArrayList<>();
+    boolean flag=false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logup);
         initialize();
 
-        login_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LogupActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        // Click logUp_btn
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = email_info.getText().toString().trim();
-                String pwd = pwd_info.getText().toString().trim();
-
-                user.createUserWithEmailAndPassword(email, pwd)
-                        .addOnCompleteListener(LogupActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(!task.isSuccessful()) {
-                                    try {
-                                        throw task.getException();
-                                    } catch(FirebaseAuthWeakPasswordException e) {
-                                        Toast.makeText(LogupActivity.this,"비밀번호가 간단합니다." ,Toast.LENGTH_SHORT).show();
-                                    } catch(FirebaseAuthInvalidCredentialsException e) {
-                                        Toast.makeText(LogupActivity.this,"E-mail 형식에 맞지 않습니다." ,Toast.LENGTH_SHORT).show();
-                                    } catch(FirebaseAuthUserCollisionException e) {
-                                        Toast.makeText(LogupActivity.this,"이미 존재하는 E-mail 입니다." ,Toast.LENGTH_SHORT).show();
-                                    } catch(Exception e) {
-                                        Toast.makeText(LogupActivity.this,"회원가입 오류" ,Toast.LENGTH_SHORT).show();
-                                    }
-                                } else {
-                                    Intent intent = new Intent(LogupActivity.this, LoginActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                }
-                            }
-                        });
-            }
-        });
+        // Button setOnClick
+        login_btn.setOnClickListener(this);
+        btn.setOnClickListener(this);
     }
 
+    @Override
     public void initialize() {
         login_btn = findViewById(R.id.go_login);
         // 분석하기
@@ -86,7 +67,85 @@ public class LogupActivity extends AppCompatActivity implements Initialize{
 
         email_info = findViewById(R.id.email_join);
         pwd_info = findViewById(R.id.pwd_join);
-        btn = findViewById(R.id.logIn_btn);
+        pwd_check = findViewById(R.id.check_password);
+        nickname_info = findViewById(R.id.nickname);
+        btn = findViewById(R.id.logUp_btn);
+
         user = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.go_login:
+                Intent intent = new Intent(LogupActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.logUp_btn:
+                doLogup(nickname_info.getText().toString().trim(), email_info.getText().toString().trim(), pwd_info.getText().toString().trim(), pwd_check.getText().toString().trim());
+                break;
+        }
+    }
+
+    private void doLogup(final String nickname, final String email, final String pwd, final String check_pwd) {
+        if (pwd.equals(check_pwd)) {
+            Toast.makeText(LogupActivity.this,"pwdequal",Toast.LENGTH_LONG).show();
+            checkNickname(nickname, user.getUid(), email, pwd);
+        } else {
+            Toast.makeText(LogupActivity.this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void checkNickname(final String newNickname, final String userUid, final String email, final String pwd){
+        DatabaseReference ref = reference.child("users").getRef();
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot){
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    User load = snapshot.getValue(User.class);
+                    assert load != null;
+                    nicknameData.add(load.nickname);
+                }
+                if(!(nicknameData.contains(newNickname))){
+                    Toast.makeText(LogupActivity.this, "writeNewUser True 들어옴", Toast.LENGTH_SHORT).show();
+                    user.createUserWithEmailAndPassword(email, pwd)
+                            .addOnCompleteListener(LogupActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (!task.isSuccessful()) {
+                                        try {
+                                            throw task.getException();
+                                        } catch (FirebaseAuthWeakPasswordException e) {
+                                            Toast.makeText(LogupActivity.this, "비밀번호가 간단합니다.", Toast.LENGTH_SHORT).show();
+                                        } catch (FirebaseAuthInvalidCredentialsException e) {
+                                            Toast.makeText(LogupActivity.this, "E-mail 형식에 맞지 않습니다.", Toast.LENGTH_SHORT).show();
+                                        } catch (FirebaseAuthUserCollisionException e) {
+                                            Toast.makeText(LogupActivity.this, "이미 존재하는 E-mail 입니다.", Toast.LENGTH_SHORT).show();
+                                        } catch (Exception e) {
+                                            Toast.makeText(LogupActivity.this, "회원가입 오류", Toast.LENGTH_SHORT).show();
+                                        }
+                                        // onComplete task if문
+                                    } else {
+                                        User user = new User(email, newNickname);
+                                        reference.child("users").child(userUid).setValue(user);
+                                        Intent intent = new Intent(LogupActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } // onComplete task else 문
+                                }// onComplete 함수
+                            }); // OnCompleteListener
+                    // 닉네임 비교 if문
+                } else{
+                    Toast.makeText(LogupActivity.this, "닉네임이 존재합니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("loadUser:onCancelled", databaseError.toException());
+            }
+        });
     }
 }
